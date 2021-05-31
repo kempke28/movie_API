ce1fe6fb5408a7567f2ac33d8aea2c66f165eb24
Akempke <akempkedev@gmail.com>
2021-05-31 01:43:04 +0100

    typo

--- models.js
+++ models.js
@@ -29,11 +29,11 @@ let usersSchema = mongoose.Schema({
     FavMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
-userSchema.statics.hashPassword = (password) => {
+usersSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 };
   
-userSchema.methods.validatePassword = function(password) {
+usersSchema.methods.validatePassword = function(password) {
 return bcrypt.compareSync(password, this.Password);
 };
 
