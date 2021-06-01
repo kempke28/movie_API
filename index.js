6e01db8abfed6c68509be56a770e7300b12c47a1
Akempke <akempkedev@gmail.com>
2021-06-01 02:57:57 +0100

    disable cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Models = require('./models'); //Brings the models or templates from the js
 
 const passport = require('passport');
 require('./passport');
-
+app.use(bodyParser.json());
 // call the models from model.js 
 
 const Movies = Models.Movies;
@@ -40,18 +40,20 @@ app.get('/documentation', (req, res) => {
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'https://movie-api-1.herokuapp.com'];
+app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:8080', 'https://movie-api-1.herokuapp.com'];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 require('./auth')(app);
 
