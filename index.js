7fbb699ff2dd9e61320a544501d60133b1bf9ea2
Akempke <akempkedev@gmail.com>
2021-06-10 20:07:33 +0100

    Temporary update
    checking

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(express.json());
 
 //Cors (Cross-Origin Resource Sharing) allow requests from other domains
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
@@ -143,16 +143,16 @@ app.post('/movies', passport.authenticate('jwt', { session: false }), (req, res)
 });
 
 
-//Get all movies from database
+//Get all movies from database    ////passport.authenticate('jwt', { session: false })
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get("/movies", function (req, res) {
   Movies.find()
-    .then((movies) => {
+    .then(function (movies) {
       res.status(201).json(movies);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
     });
 });
 
