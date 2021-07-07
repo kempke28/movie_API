b93f9e3d13f67fb8422e7d3d482a902477ab50aa
Akempke <akempkedev@gmail.com>
2021-07-07 10:50:11 +0100

    Cors
    uptdated

--- index.js
+++ index.js
@@ -38,18 +38,9 @@ app.use(express.json());
 
 //Cors (Cross-Origin Resource Sharing) allow requests from other domains
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', http://localhost:1" 'https://my-flix-movies.netlify.app/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 
 app.get("/", (req, res) => {
